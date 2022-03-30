6a10ece982e5b0c052bd8d35040ec912c84ffb39
Bandzyrka <werstaptor@gmail.com>
2022-03-30 12:48:20 +0200

    Added footer, expanded homepage with buy homes section

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import {FooterContainer} from './footer.styles'
+const Footer = () => (
+    <FooterContainer>
+        2022 Houser, Inc.
+    </FooterContainer>
+)
+export default Footer
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,11 +5,9 @@ import PropertiesPreview from '../../components/properties-preview/propertiesPre
 import PROPERTIES_DATA from "./PROPERTIES_DATA"
 
 import NavBar from "../../components/nav-bar/nav-bar.component"
-
+import Footer from "../../components/footer/footer.component"
 const HomePage = () => 
-{
-    console.log(PROPERTIES_DATA.hits)
-return(
+(
     <HomePageContainer>
         <NavBar />
         <Banner 
@@ -20,7 +18,16 @@ return(
             imageUrl="https://www.parkcityvacationrentals.com/wp-content/uploads/2020/01/Rental-Homes-in-Park-City-Utah-1.jpeg"
         />
         <PropertiesPreview properties={PROPERTIES_DATA.hits}/>
+        <Banner 
+            title="BUY A HOME"
+            subtitle="FIND YOUR DREAM HOME"
+            description="Explore from Apartments, builder floors, villas and more"
+            buttonText="EXPLORE BUYING"
+            imageUrl="https://www.parkcityvacationrentals.com/wp-content/uploads/2020/01/Rental-Homes-in-Park-City-Utah-1.jpeg"
+        />
+        <PropertiesPreview properties={PROPERTIES_DATA.hits}/>
+        <Footer />
     </HomePageContainer>
-    )
-}
+)
+
 export default HomePage;
